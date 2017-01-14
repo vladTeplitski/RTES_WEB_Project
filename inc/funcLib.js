903a48ab8465fd9028f54acd06329f65de3c038d
Vlad <vl4d89@gmail.com>
2017-01-14 12:29:10 +0200

    New

---
+++ inc/funcLib.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {   //after page loads
+
+
+    //controlling the textContainers on main page
+    $(".resetApp").on("click", function () {
+        $(".textContainer").hide();
+        $(".chapter1").fadeIn();
+    });
+    $(".btn1").on("click", function () {
+        $(".textContainer").hide();
+        $(".chapter2").fadeIn();
+    });
+    $(".btn2").on("click", function () {
+        $(".textContainer").hide();
+        $(".chapter3").fadeIn();
+    });
+    $(".btn3").on("click", function () {
+        $(".textContainer").hide();
+        $(".chapter4").fadeIn();
+    });
+    $(".btn4").on("click", function () {
+        $(".textContainer").hide();
+        $(".chapter5").fadeIn();
+    });
+    $(".btn5").on("click", function () {
+        $(".textContainer").hide();
+        $(".chapter6").fadeIn();
+    });
+    $(".logjs").on("click", function () {
+        $(".loginBox").toggle();
+    });
+    $(".closeLoginContainer").on("click", function () {
+        $(".loginBox").hide();
+    });
+    $(".regjs").on("click", function () {
+        $(".registerBox").toggle();
+    });
+    $(".closeRegContainer").on("click", function () {
+        $(".registerBox").hide();
+    });
+
+
+
+});
