9afaab1cd2b3774f0f760da497f26276d470a810
Vlad Teplitski <vl4d89@gmail.com>
2017-04-12 17:11:53 +0300

    AWS database

--- RTESWebProjectMVC/Models/rtesEntities1.Designer.cs
+++ RTESWebProjectMVC/Models/rtesEntities1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Coding\WebForms2MVC\RTESWebProjectMVC\RTESWebProjectMVC\Models\rtesEntities1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Coding\RTES_WEB_Project\RTESWebProjectMVC\Models\rtesEntities1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
