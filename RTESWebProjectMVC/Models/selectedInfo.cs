7e874e938d06822babef50581a909650678419f2
Vlad Teplitski <vl4d89@gmail.com>
2017-04-14 12:32:05 +0300

    selectedinfo

--- RTESWebProjectMVC/Models/selectedInfo.cs
+++ RTESWebProjectMVC/Models/selectedInfo.cs
@@ -6,7 +6,7 @@ using System.Web;
 namespace RTESWebProjectMVC.Models
 {
     public class selectedInfo
-    {
+    {//new
         public int id { get; set; }
         public String name { get; set; }
     }
